269af35c87338be2684776e6cc2043c4d3afd1d9
Rafael Jesus <rafael.jesus@marlabs.com>
2024-08-28 10:40:16 -0300

    teste

---
+++ generate-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the prefix for the secrets
+PREFIX="ENV_VAR_"
+
+# Create or overwrite the .env file
+> .env
+
+# Loop over all environment variables
+for SECRET_NAME in $(printenv | cut -d= -f1); do
+    SECRET_VALUE=${!SECRET_NAME}
+    echo "${SECRET_NAME#${PREFIX}}=${SECRET_VALUE}" >> .env
+done
